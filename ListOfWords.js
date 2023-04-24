b63c94d06912bf344f85830eefa0f9bcc29870de
enicolel <enicolel@villanova.edu>
2023-04-24 15:09:20 -0400

    Update ListOfWords.js

--- ListOfWords.js
+++ ListOfWords.js
@@ -3,16 +3,16 @@ import { StyleSheet, Text, View, FlatList, ScrollView, TouchableOpacity } from '
 
 export default function ListOfWords({ navigation, route }) {
     
-    const words = [{Word: "Fabulous", WordDescription: "extraordinary, especially extraordinarily large"},
-    {Word: "Sesquicentennial", WordDescription: "relating to the one-hundred-and-fiftieth anniversary of a significant event"},
-    {Word: "Palaeoanthropology", WordDescription: "The study of early humans"},
-    {Word: "Volumenometer", WordDescription: "The instrument that determines volume of a solid"},
-    {Word: "Otorhinolaryngology", WordDescription: "The study of ear, nose, and throat"},
-    {Word: "Unconsentaneous", WordDescription: "To be in disagreement"},
-    {Word: "Gephydrophobia", WordDescription: "The fear of crossing bridges"},
-    {Word: "Jurisprudential", WordDescription: "Pertaining to the science of jurdical law"},
-    {Word: "Xerophthalmia", WordDescription: "Dry eye syndrome"},
-    {Word: "Ultramicroscope", WordDescription: "Instrument for viewing objects that are difficult to see by the naked eye"}];
+    const words = [{key: '001', Word: "Fabulous", WordDescription: "extraordinary, especially extraordinarily large"},
+    {key: '002', Word: "Sesquicentennial", WordDescription: "relating to the one-hundred-and-fiftieth anniversary of a significant event"},
+    {key: '003', Word: "Palaeoanthropology", WordDescription: "The study of early humans"},
+    {key: '004', Word: "Volumenometer", WordDescription: "The instrument that determines volume of a solid"},
+    {key: '005', Word: "Otorhinolaryngology", WordDescription: "The study of ear, nose, and throat"},
+    {key: '006', Word: "Unconsentaneous", WordDescription: "To be in disagreement"},
+    {key: '007', Word: "Gephydrophobia", WordDescription: "The fear of crossing bridges"},
+    {key: '008', Word: "Jurisprudential", WordDescription: "Pertaining to the science of jurdical law"},
+    {key: '009', Word: "Xerophthalmia", WordDescription: "Dry eye syndrome"},
+    {key: '0010', Word: "Ultramicroscope", WordDescription: "Instrument for viewing objects that are difficult to see by the naked eye"}];
   
   const [listWords, setListWords] = useState(words)
   
