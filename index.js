fe190472ccc42608e14442e5909d007f7bb31f10
iguiliano <130933949+iguiliano@users.noreply.github.com>
2023-04-26 14:14:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./styles.css";
 import App from "./App";
 
+import img0 from "./images/0.jpg";
+import img1 from "./images/1.jpg";
+import img2 from "./images/2.jpg";
+import img3 from "./images/3.jpg";
+import img4 from "./images/4.jpg";
+import img5 from "./images/5.jpg";
+import img6 from "./images/6.jpg";
+
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
